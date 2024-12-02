af9a14092525a14533ffcf21f0930a0e1f06964e
Bitsu <bistua.mamo@gmail.com>
2024-12-02 11:02:57 -0600

    Complete: Part one of day two

---
+++ src/main/java/DayTwo/PartOne.java
@@ -0,0 +1,47 @@
+package DayTwo;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class PartOne {
+    public static void main(String[] args) {
+        String input;
+        try {
+            input = Files.readString(Path.of("src", "main", "java", "DayTwo", "input.txt"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        int res = 0;
+        for(String line: input.split("\n")) {
+            line = line.strip();
+            var in = line.split(" ");
+            int prev = 0;
+            boolean isSafe = true;
+            for(int i = 0; i < in.length - 1; i++) {
+                Integer firstNum = Integer.parseInt(in[i]);
+                Integer secondNum = Integer.parseInt(in[i + 1]);
+
+                if (Math.abs(firstNum - secondNum) > 3) {
+                    isSafe = false;
+                    break;
+                }
+                if( i == 0){
+                    prev = firstNum - secondNum;
+                    continue;
+                }
+                int cur = firstNum - secondNum;
+                if((prev >= 0 && cur <= 0) || (prev <= 0 && cur >= 0)) {
+                    isSafe = false;
+                    break;
+                }
+            }
+            if(isSafe) {
+                res++;
+            }
+        }
+        System.out.println(res);
+    }
+}
